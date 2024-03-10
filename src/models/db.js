cf118c5aa54763d2348909cf594dda4b034396b2
Laura Murray <20104312@mail.wit.ie>
2024-03-10 16:24:43 +0000

    Uservand location testing working

--- src/models/db.js
+++ src/models/db.js
@@ -8,6 +8,9 @@ import { imageJsonStore } from "./json/image-json-store.js";
 import { reviewJsonStore } from "./json/review-json-store.js";
 import { locationJsonStore } from "./json/location-json-store.js";
 import { detailsJsonStore } from "./json/details-json-store.js";
+import { userMemStore } from "./mem/user-mem-store.js";
+import { locationMemStore } from "./mem/location-mem-store.js";
+import { detailsMemStore } from "./mem/details-mem-store.js";
 
 
 export const db = {
@@ -17,11 +20,19 @@ export const db = {
   locationStore: null,
   detailStore: null,
 
-  init() {
-    this.userStore = userJsonStore;
-    this.imageStore = imageJsonStore;
-    this.reviewStore = reviewJsonStore;
-    this.locationStore = locationJsonStore;
-    this.detailStore = detailsJsonStore;
+  init(storeType) {
+    switch (storeType) {
+      case "json":
+        this.userStore = userJsonStore;
+        this.imageStore = imageJsonStore;
+        this.reviewStore = reviewJsonStore;
+        this.locationStore = locationJsonStore;
+        this.detailStore = detailsJsonStore;
+        break;
+      default:
+        this.userStore = userMemStore;
+        this.locationStore = locationMemStore;
+        this.detailStore = detailsMemStore;
+    }
   },
 };
